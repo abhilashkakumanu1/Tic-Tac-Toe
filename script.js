d6f5f0aaaad0ad796334b5fa3b90146e0e0da06a
Abhi <abhilashkakumanu1@gmail.com>
2020-04-21 13:22:56 +0530

    added color

--- script.js
+++ script.js
@@ -5,19 +5,30 @@ function check(){
     for(var i=0;i<3;i++){
         //Checking Rows
         if(ticTacToe[3*i]==="X" && ticTacToe[3*i+1]==="X" && ticTacToe[3*i+2]==="X"){
-            
-            console.log("Row");
+            for(var j=0;j<3;j++){
+                document.getElementById(3*i+j).style.color="green";
+            }
             return;
         }
         //Checking Columns;
         if(ticTacToe[i]==="X" && ticTacToe[i+3]==="X" && ticTacToe[i+6]==="X"){
-            console.log("Column");
+            for(var j=0;j<3;j++){
+                document.getElementById(i+3*j).style.color="green";
+            }
             return;
-        }
+        }  
     }
     //Checking for diagonals
-    if((ticTacToe[0]==="X" && ticTacToe[4]==="X" && ticTacToe[8]==="X") || (ticTacToe[2]==="X" && ticTacToe[4]==="X" && ticTacToe[6]==="X")){
-        console.log("Diagonal");
+    if(ticTacToe[0]==="X" && ticTacToe[4]==="X" && ticTacToe[8]==="X"){
+        for(var j=0;j<9;j+=4){
+            document.getElementById(j).style.color="green"
+        }
+        return;
+    }
+    if(ticTacToe[2]==="X" && ticTacToe[4]==="X" && ticTacToe[6]==="X"){
+        for(var j=0;j<7;j+=2){
+            document.getElementById(j).style.color="green"
+        }
         return;
     }
 }
